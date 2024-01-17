b7176ebb8063959391e12cddd1445735d35179c5
ikecruz <ikedinobicruz7@gmail.com>
2024-01-17 23:31:26 +0000

    cors

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 });
 
